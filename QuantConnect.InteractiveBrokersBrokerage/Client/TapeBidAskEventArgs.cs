a4cd30740b753323bad976ea87ff8635fad8be94
laur <laur.aliste@gmail.com>
2024-07-18 13:25:25 +0200

    RequestTapeTopOfBook: to make sure our tape has correct top-of-book

---
+++ QuantConnect.InteractiveBrokersBrokerage/Client/TapeBidAskEventArgs.cs
@@ -0,0 +1,65 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+
+namespace QuantConnect.Brokerages.InteractiveBrokers.Client
+{
+    /// <summary>
+    /// Event arguments class for the <see cref="InteractiveBrokersClient.Tape"/> event
+    /// 
+    /// see https://ibkrcampus.com/ibkr-api-page/twsapi-doc/#receive-tick-data
+    /// </summary>
+    public sealed class TapeBidAskEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The request's unique identifier.
+        /// </summary>
+        public int TickerId { get; }
+
+        /// real-time tick timestamp
+        /// </summary>
+        public long Time { get; }
+
+        /// <summary>
+        /// </summary>
+        public double BidPrice { get; }
+        
+        /// <summary>
+        /// </summary>
+        public double AskPrice { get; }
+        
+        /// <summary>
+        /// </summary>
+        public decimal BidSize { get; }
+        
+        /// <summary>
+        /// </summary>
+        public decimal AskSize { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TapeBidAskEventArgs"/> class
+        /// </summary>
+        public TapeBidAskEventArgs(int reqId, long time, double bidPrice, double askPrice, decimal bidSize, decimal askSize)
+        {
+            TickerId = reqId;
+            Time = time;
+            BidPrice = bidPrice;
+            AskPrice = askPrice;
+            BidSize = bidSize;
+            AskSize = askSize;
+        }
+    }
+}
